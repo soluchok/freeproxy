c9ac27f4511a7d079c594e8d863efc1c24f2f070
zzell <sukhodolskyi.97@gmail.com>
2019-01-13 19:15:03 +0200

    add coolproxy provider

---
+++ providers/coolproxy.go
@@ -0,0 +1,130 @@
+package providers
+
+import (
+	"bytes"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"net/http"
+	"regexp"
+	"time"
+
+	"github.com/moovweb/gokogiri"
+)
+
+type CoolProxy struct {
+	proxyList  []string
+	lastUpdate time.Time
+}
+
+func NewCoolProxy() *CoolProxy {
+	return new(CoolProxy)
+}
+
+const coolProxyURL = `https://www.cool-proxy.net/proxies/http_proxy_list/sort:score/direction:desc`
+
+func (c *CoolProxy) Load(body []byte) ([]string, error) {
+	if time.Now().Unix() >= c.lastUpdate.Unix()+(60*5) {
+		c.proxyList = make([]string, 0, 0)
+	}
+
+	if len(c.proxyList) != 0 {
+		return c.proxyList, nil
+	}
+
+	if body == nil {
+		var err error
+		if body, err = c.MakeRequest(); err != nil {
+			return nil, err
+		}
+	}
+
+	doc, err := gokogiri.ParseHtml(body)
+	if err != nil {
+		return nil, err
+	}
+
+	defer doc.Free()
+
+	ips, err := doc.Search(`//*[@id="main"]/table/tr/td[1][not(@colspan)]/script`)
+	if err != nil {
+		return nil, err
+	}
+
+	ports, err := doc.Search(`//*[@id="main"]/table/tr/td[2]`)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(ips) == 0 {
+		return nil, errors.New("ip not found")
+	}
+
+	if len(ips) != len(ports) {
+		return nil, errors.New("len port not equal ip")
+	}
+
+	r := regexp.MustCompile(`"(.*?[^\\])"`)
+
+	for i, ip := range ips {
+		raw := r.FindStringSubmatch(ip.Content())
+		if len(raw) != 2 {
+			continue
+		}
+
+		decoded, err := base64.StdEncoding.DecodeString(string(bytes.Map(rot13, []byte(raw[1]))))
+		if err != nil {
+			continue
+		}
+
+		c.proxyList = append(c.proxyList, fmt.Sprintf("%s:%s", decoded, ports[i].Content()))
+	}
+
+	return c.proxyList, nil
+}
+
+func (c *CoolProxy) MakeRequest() ([]byte, error) {
+	var (
+		body   bytes.Buffer
+		client = &http.Client{
+			Timeout:   time.Second * 10,
+			Transport: TransportMakeRequest,
+		}
+	)
+
+	req, err := http.NewRequest(http.MethodGet, coolProxyURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		e := res.Body.Close()
+		if err != nil {
+			err = e
+		}
+	}()
+
+	_, err = body.ReadFrom(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body.Bytes(), err
+}
+
+func rot13(x rune) rune {
+	capital := x >= 'A' && x <= 'Z'
+	if !capital && (x < 'a' || x > 'z') {
+		return x
+	}
+	x += 13
+	if capital && x > 'Z' || !capital && x > 'z' {
+		x -= 26
+	}
+	return x
+}
